fc87af7c72aee1830941e4bee01dbb907dc5b935
lertora@eurovision.net <lertora@eurovision.net>
2020-04-26 02:14:34 +0200

    Removed ionicon references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { AvatarDialogComponent } from './avatar-dialog/avatar-dialog.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { EditUserResolver } from './edit-user/edit-user.resolver';
+import { NewUserComponent } from './new-user/new-user.component';
+import { HomeComponent } from './home/home.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { FirebaseService } from './services/firebase.service';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarDialogComponent,
+    EditUserComponent,
+    NewUserComponent,
+    HomeComponent
   ],
+  entryComponents: [AvatarDialogComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSliderModule,
+    MatDialogModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService, EditUserResolver],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAp1D_cp-4YhVo4tJBRmtgY-13yosCq1KQ",
+    authDomain: "letsplay-99d2c.firebaseapp.com",
+    databaseURL: "https://letsplay-99d2c.firebaseio.com",
+    projectId: "letsplay-99d2c",
+    storageBucket: "letsplay-99d2c.appspot.com",
+    messagingSenderId: "329107847528",
+  }
 };
 
 /*
