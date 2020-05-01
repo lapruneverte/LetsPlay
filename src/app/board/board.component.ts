5b8a9ac7695586aebd609b767299f697a07c645d
georgecardenas <warpmodder@gmail.com>
2020-05-01 15:19:36 +0200

    Board con background image

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
 import { BoardModel } from '../core/models/board.model';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -11,6 +11,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 export class BoardComponent implements OnInit {
 
   boardGame: BoardModel = new BoardModel();
+  imgWidth: number;
+  imgHeight: number;
   tokens: any[];
   constructor(
       public firebaseService: FirebaseService,
@@ -28,6 +30,15 @@ export class BoardComponent implements OnInit {
         this.boardGame.name = result[0].name;
         this.boardGame.link = result[0].link;
         this.tokens = result[0].tokens;
+
+        let self = this;
+        let img = new Image();
+        img.onload = function(event:Event){
+          let loadedImage: any = event.target;
+          self.imgWidth = loadedImage.width;
+          self.imgHeight = loadedImage.height;
+        };
+        img.src = result[0].link;
       } else {
         console.log("Board Component: found 0 or more than 1 board games with that name...");
       }
