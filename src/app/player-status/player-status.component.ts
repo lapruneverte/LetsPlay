1a2aa7c38c153b97bd81c7ed97666312a38f9d49
georgecardenas <warpmodder@gmail.com>
2020-05-07 20:16:31 +0200

    Animations?

--- src/app/player-status/player-status.component.ts
+++ src/app/player-status/player-status.component.ts
@@ -2,19 +2,28 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { RoomModel } from '../core/models/room.model';
 import { ZoomModel } from '../core/models/zoom.model';
 import { MatCardModule } from '@angular/material/card';
+import { trigger, transition, state, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-player-status',
   templateUrl: './player-status.component.html',
-  styleUrls: ['./player-status.component.scss']
+  styleUrls: ['./player-status.component.scss'],
+  animations: [
+    trigger('simpleFadeAnimation', [
+      transition(':increment', [style({ opacity: 0 }), animate('2000ms', style({ opacity: 1 }))]),
+      transition(':decrement', [style({ opacity: 0 }), animate('2000ms', style({ opacity: 1 }))])
+    ])
+  ]
 })
 export class PlayerStatusComponent implements OnInit {
   @Input() room: RoomModel;
   @Output() onZoom = new EventEmitter<ZoomModel>();
-  
+  playerId: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.playerId = sessionStorage.getItem('playerId');
   }
   
   zoomImage(event: MouseEvent, playerIndex: number, cardIndex: number) {
@@ -30,5 +39,10 @@ export class PlayerStatusComponent implements OnInit {
     zoomData.src = this.room.players[playerIndex].preview[cardIndex].link;
 
     this.onZoom.emit(zoomData);
+  }
+
+  roomPlayers() {
+    //return this.room.players.filter(player => player.playerId != this.playerId);
+    return this.room.players;
   }
 }
